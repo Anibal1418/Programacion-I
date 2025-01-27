c62073d89e4c6f3c44ae46f72c7ae326c1677342
Luis Anibal <aniballuis180@gmail.com>
2025-01-27 17:03:56 -0400

    Yes

--- TestProject/Testing.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-int x;
-
-x = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine($"{x} is the number you inputted");
