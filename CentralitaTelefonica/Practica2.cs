acb3e1716d1b759b61b92adb1c7f53a1a7b4aaae
Anibal1418 <144165862+Anibal1418@users.noreply.github.com>
2025-04-04 14:24:39 -0400

    Add files via upload

--- CentralitaTelefonica/Practica2.cs
+++ CentralitaTelefonica/Practica2.cs
@@ -3,6 +3,7 @@
     public static void Main()
     {
         Centralita centralita = new Centralita();
+        var inserter = new MySQLInserter();
 
         LlamadaLocal llamada1 = new LlamadaLocal("809-123-4567", "849-333-1234", 120);
         LlamadaProvincial llamadaP1 = new LlamadaProvincial("829-391-0654", "800-900-1000", 63, 3);
@@ -17,6 +18,13 @@
         centralita.registrarLlamada(llamadaP1);
         centralita.registrarLlamada(llamadaP2);
         centralita.registrarLlamada(llamadaP3);
+
+        inserter.InsertLlamada(llamada1);
+        inserter.InsertLlamada(llamada2);
+        inserter.InsertLlamada(llamada3);
+        inserter.InsertLlamada(llamadaP1);
+        inserter.InsertLlamada(llamadaP2);
+        inserter.InsertLlamada(llamadaP3);
 
         Console.WriteLine($"Al final de la jornada, la centralita registró {centralita.getTotalLlamadas()} llamadas con un total facturado de {centralita.getTotalFacturado()} pesos.");
 
